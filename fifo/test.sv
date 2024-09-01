89e63af22978fa44478ccd2ae776df975eda47ee
Josue096 <josu3qb@gmail.com>
2024-09-01 01:08:51 -0600

    lec_esc17

--- fifo/test.sv
+++ fifo/test.sv
@@ -52,7 +52,7 @@ class test #(parameter width = 16, parameter depth = 8);
         test_agent_mbx.put(instr_agent);
         $display("[%g] Test: Enviada la cuarta instruccion al agente secuencia %g de transaccion_aleatoria", $time, num_transacciones);
 
-        #1000
+        #2000
         $display("[%g] Test: Se alcanza el tiempo limite de la prueba", $time);
         instr_sb = retardo_promedio;
         test_sb_mbx.put(instr_sb);
