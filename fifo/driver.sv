792bd0b811bf0323b38eccd49ebfe98fd7b6c2d6
Josue096 <josu3qb@gmail.com>
2024-08-31 21:49:37 -0600

    lec_esc3

--- fifo/driver.sv
+++ fifo/driver.sv
@@ -49,13 +49,17 @@ class driver #(parameter width = 16);
                 end
 
                 lectura_escritura: begin
+                    
                     transaction.dato = vif.dato_out;
-                    vif.push = 1;
                     transaction.tiempo = $time;
                     @(posedge vif.clk);
                     vif.pop = 1;
                     drv_chkr_mbx.put(transaction);
-                    transaction.print("Driver: Transaaacion ejecutada");
+                    
+                    vif.push = 1;
+                    transaction.tiempo = $time;
+                    drv_chkr_mbx.put(transaction);
+                    transaction.print("Driver: Transaccion ejecutada");
                 end
 
                 reset: begin
