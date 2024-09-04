3a0a4a2a2ed82cece560b889c229a40d4a859247
Josue096 <josu3qb@gmail.com>
2024-09-03 21:44:46 -0600

    pruebaG

--- fifo/checker.sv
+++ fifo/checker.sv
@@ -80,7 +80,7 @@ class checker_c #(parameter width = 16, parameter depth = 8);
                         
                         
                     end else begin 
-                        to_sb.dato_enviado = auxiliar.dato;
+                        //to_sb.dato_enviado = auxiliar.dato;
                         to_sb.tiempo_pop = transaccion.tiempo;
                         to_sb.underflow = 1;
                         to_sb.print("Checker: Underflow");
