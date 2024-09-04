263780402aac5ce49f15d171587aceaad53e7a39
Josue096 <josu3qb@gmail.com>
2024-09-03 21:29:33 -0600

    prueba26

--- fifo/driver.sv
+++ fifo/driver.sv
@@ -50,9 +50,10 @@ class driver #(parameter width = 16);
 
                 lectura_escritura: begin  
                     
-                    vif.push = 1;
+                    
                     @(posedge vif.clk);                  
                     vif.pop = 1;
+                    vif.push = 1;
                     transaction.tiempo = $time;
                     //transaction.dato = vif.dato_out;
                     drv_chkr_mbx.put(transaction);
