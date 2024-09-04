cb7a7d7af8711237fc1934785a7c7217af7f9d6b
Josue096 <josu3qb@gmail.com>
2024-09-03 19:27:06 -0600

    prueba24

--- fifo/test.sv
+++ fifo/test.sv
@@ -52,7 +52,7 @@ class test #(parameter width = 16, parameter depth = 8);
         
 
         ambiente_inst.agent_inst.ret_spec = 3;
-        ambiente_inst.agent_inst.tpo_spec = lectura_escritura;
+        ambiente_inst.agent_inst.tpo_spec = escritura;
         ambiente_inst.agent_inst.dto_spec = {width/4{4'h5}};
         instr_agent = trans_especifica;
         test_agent_mbx.put(instr_agent);
