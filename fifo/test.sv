e75caae7747dbb9b4957dac4132d368c57ecba53
Josue096 <josu3qb@gmail.com>
2024-09-03 19:39:34 -0600

    prueba24

--- fifo/test.sv
+++ fifo/test.sv
@@ -42,7 +42,7 @@ class test #(parameter width = 16, parameter depth = 8);
         $display("[%g] Test: Enviada la segunda instruccion al agente transaccion aleatoria", $time);
         */
         
-        /*for (int i = 0; i < num_transacciones; i++) begin
+        for (int i = 0; i < num_transacciones; i++) begin
             ambiente_inst.agent_inst.ret_spec = 3;
             ambiente_inst.agent_inst.tpo_spec = escritura;
             ambiente_inst.agent_inst.dto_spec = {width/4{4'h5}};
@@ -50,9 +50,9 @@ class test #(parameter width = 16, parameter depth = 8);
             test_agent_mbx.put(instr_agent);
             $display("[%g] escrt", $time);
         end 
-        */
         
-
+        
+        wait(i == 10); 
         ambiente_inst.agent_inst.ret_spec = 5;
         ambiente_inst.agent_inst.tpo_spec = lectura_escritura;
         ambiente_inst.agent_inst.dto_spec = {width/4{4'h5}};
